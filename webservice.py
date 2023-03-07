fbdd5deb41b5c8338b8a2e0d7a0be6444fe64c3e
pumba-dev <pumbadeveloper@gmail.com>
2023-03-07 13:41:19 -0300

    Feat: create Message class

---
+++ webservice.py
@@ -0,0 +1,6 @@
+class Message:
+    def __init__(self, sender, receiver, subject, content):
+        self.sender = sender
+        self.receiver = receiver
+        self.subject = subject
+        self.content = content  
