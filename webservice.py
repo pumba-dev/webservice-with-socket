56816f4af8d631be1983c613dbc98f53da8f7b14
pumba-dev <pumbadeveloper@gmail.com>
2023-03-07 13:43:09 -0300

    Feat: create a MailService class

--- webservice.py
+++ webservice.py
@@ -4,3 +4,11 @@ class Message:
         self.receiver = receiver
         self.subject = subject
         self.content = content  
+
+class MailService:
+    def __init__(self):
+        self.messages = []
+
+    def sendMessage(self, message):
+        self.messages.append(message)
+        return {"Mensagem enviada com sucesso."}
